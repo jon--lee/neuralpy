6b26e1642d497040f4965514a1c3c8559f57f589
Jonathan Lee <123abcjonathanlee@gmail.com>
2015-06-29 23:26:52 -0700

    updated __init__.py to include other functions of neural.py

--- neuralpy/__init__.py
+++ neuralpy/__init__.py
@@ -1,1 +1,1 @@
-from neural import Network
+from neural import Network, output, bcolors, cost_function, cost_derivative, cost_function_vec, cost_derivative_vec, sigmoid, sigmoid_prime, sigmoid_vec, sigmoid_prime_vec
