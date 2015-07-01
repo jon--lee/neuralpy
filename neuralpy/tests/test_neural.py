fd9a78fd1404dc4b56573094ce60ce13d7787651
Jonathan Lee <123abcjonathanlee@gmail.com>
2015-07-01 15:03:46 -0700

    added unit test for cost function

--- neuralpy/tests/test_neural.py
+++ neuralpy/tests/test_neural.py
@@ -1,4 +1,22 @@
 import unittest
 import neuralpy
 
-neuralpy.output("yo some code to test");
+class associatingElementsTest(unittest.TestCase):
+
+	def test_cost_function(self):
+		#tuples are arranged as ((input1, input2), expected_output)
+		cases = [((1.0, 1.0), 0.0),
+				((1.0, 0.5), 0.125),
+				((0.5, 1.0), 0.125),
+				((0.25, 0.0), 0.03125),
+				((0.0, 0.25), 0.03125)
+			]	
+		for case in cases:
+			input1 = case[0][0]
+			input2 = case[0][1]
+			output = case[1]
+			self.assertEqual(neuralpy.cost_function(input1, input2), output)
+
+
+if __name__ == '__main__':
+	unittest.main()
