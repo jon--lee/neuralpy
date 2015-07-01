863ccffa8dafed98fe76b3ad59388c62f02f1fa6
Jonathan Lee <123abcjonathanlee@gmail.com>
2015-07-01 15:22:43 -0700

    update network and variables, create example test file

--- neuralpy/neural.py
+++ neuralpy/neural.py
@@ -3,7 +3,7 @@ import random
 import matplotlib.pyplot as plt
 
 def output(x = ""):
-	print bcolors.OKGREEN + str(x) + bcolors.ENDC
+	print bcolors.WARNING + str(x) + bcolors.ENDC
 
 class bcolors:
 	HEADER = '\033[95m'
@@ -38,14 +38,14 @@ sigmoid_prime_vec = np.vectorize(sigmoid_prime)
 
 class Network():
 	
-	def __init__(self, sizes):
-		self.num_layers = len(sizes)
+	def __init__(self, layers):
+		self.num_layers = len(layers)
 
 		self.cost_plots = []															# list of lists that correspond to the y-axis of the cost function plot
 
-		self.sizes = sizes
-		self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-		self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+		self.layers = layers
+		self.biases = [np.random.randn(y, 1) for y in layers[1:]]
+		self.weights = [np.random.randn(y, x) for x, y in zip(layers[:-1], layers[1:])]
 
 	def feedforward(self, a):
 		for b, w in zip(self.biases, self.weights):
@@ -53,25 +53,29 @@ class Network():
 		return a
 
 	def show_costs(self):
-		x = xrange(0, len(self.cost_plots[0]), 1)
 		for plot in self.cost_plots:
+			x = xrange(0, len(plot), 1)
 			plt.plot(x, plot)
 		plt.show()
 
-	def gradient_descent(self, training_data, epochs, eta, mini_batch_size = 0, monitor_cost = False):
+	def gradient_descent(self, training_data, epochs, eta, batch_length = 0, monitor_cost = False):
 		n = len(training_data)
 		eta = float(eta)
-		if(mini_batch_size <= 0):
-			mini_batch_size = n
+		if(batch_length <= 0):
+			batch_length = n
 		if monitor_cost:
-			self.cost_plots.append([])
-			num_cost_plots = len(self.cost_plots)
+			self.cost_plots.append([])					# if were tracking the cost, create a new plot to track this descent
+			num_cost_plots = len(self.cost_plots)		# number of cost plots that we have so far
 
+		# iterate over the number of epochs and update every weight/bias for all training sets in the batch
 		for j in xrange(epochs):
 			random.shuffle(training_data)
-			batch = training_data[:mini_batch_size]
+			batch = training_data[:batch_length]
 			self.update_batch(training_data, eta)
 
+			# if there is a request to monitor the cost, we want to monitor the cost after 
+			# the update form each epoch. Rather than recalculating the feed forward, it would be better
+			# to have that data in a dictionary
 			if(monitor_cost):
 				cost_components = []
 				for x, y in batch:
@@ -104,27 +108,27 @@ class Network():
 
 		# feedforward
 		activation = x
-		activations = [x] # list to store all the activations, layer by layer
-		sums = [] # list to store all the z vectors, layer by layer
+		activations = [x] 	# list to store all the activations, layer by layer
+		sums = [] 			# list to store all the u vectors, layer by layer
 		for b, w in zip(self.biases, self.weights):
-			z = np.dot(w, activation)+b
-			sums.append(z)
-			activation = sigmoid_vec(z)
+			u = np.dot(w, activation)+b
+			sums.append(u)
+			activation = sigmoid_vec(u)
 			activations.append(activation)
 
 		# backward pass
 		# sigmoid prime vector can be optimized as it is just an equation dependent on the components
 		# of the activation layer (the sigmoid part has already been computed, no need to compute it again)
-		delta = cost_derivative(activations[-1], y) * sigmoid_prime_vec(sums[-1])
-		partial_biases[-1] = delta
-		partial_weights[-1] = np.dot(delta, activations[-2].transpose())
+		epsilon = cost_derivative(activations[-1], y) * sigmoid_prime_vec(sums[-1])
+		partial_biases[-1] = epsilon
+		partial_weights[-1] = np.dot(epsilon, activations[-2].transpose())
 
 		for l in xrange(2, self.num_layers):
-			z = sums[-l]
-			spv = sigmoid_prime_vec(z)
-			delta = np.dot(self.weights[-l+1].transpose(), delta) * spv
-			partial_biases[-l] = delta
-			partial_weights[-l] = np.dot(delta, activations[-l-1].transpose())
+			u = sums[-l]
+			spv = sigmoid_prime_vec(u)
+			epsilon = np.dot(self.weights[-l+1].transpose(), epsilon) * spv
+			partial_biases[-l] = epsilon
+			partial_weights[-l] = np.dot(epsilon, activations[-l-1].transpose())
 		return (partial_biases, partial_weights)
 
 
